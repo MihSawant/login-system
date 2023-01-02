de92e478bc864963a975cfe3fe8f780392e53565
Mihir Sawant <mihirssawant06@gmail.com>
2023-01-02 23:59:03 +0530

    added services as api queries

---
+++ services/user_service.js
@@ -0,0 +1,79 @@
+const Joi = require('joi');
+const db = require('../db/database_connection');
+
+function insertNewUser(req, res){
+    let user_email = String(req.body.email);
+    let password = String(req.body.password);
+    
+  
+        
+ 
+
+               // check does user already exists
+    let userCheck = `SELECT COUNT(email) AS user_count FROM users WHERE email = '${user_email}'`;
+
+    db.myConnection.query(userCheck, (error, result) =>{
+        var first = Object.values(result)[0];
+        var count = Object.values(first)[0];  
+        if(error){
+            throw error;
+        } 
+        else if(count > 0){
+                res.status(409);
+                return res.json({
+                    "error" : true,
+                    "message" : "User Already Exists !"
+                });
+        } 
+        else{
+            let insertQuery = `INSERT INTO users(email, password) VALUES('${user_email}', '${password}')`;
+            db.myConnection.query(insertQuery, (error, result)=>{
+                if(error) {
+                    throw error;
+                } else{
+                   return res.json({
+                            "message" : "Record Inserted Successfully !"
+                });
+
+                }
+                
+            });
+        }
+    });
+
+
+}
+
+
+function checkUser(req, res){
+    let user_email = String(req.body.email);
+    var user = `SELECT password FROM users WHERE email = '${user_email}'`;
+    let enteredPass = String(req.body.password);
+   
+   db.myConnection.query(user, (error, result)=>{
+    var first = Object.values(result)[0];
+    var pass = Object.values(first)[0];
+
+    if(error){
+        throw error;
+    } else{
+        if(String(pass) === enteredPass){
+            res.status(200);
+            res.json({
+                "message" : "Password Match Success"
+            });
+        } else{
+            res.status(400);
+            res.json({
+                "error" : true,
+                "message" : "Password Match Fail"
+            });
+        }
+    }
+   });
+}
+    
+
+module.exports = {
+    insertNewUser, checkUser
+}
