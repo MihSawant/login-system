d0856c0a766ed70cbf28581c9d18439df62ca209
Mihir Sawant <mihirssawant06@gmail.com>
2023-01-02 23:59:18 +0530

    endpoints created

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
-
 const PORT = 8000;
+const bodyParser = require('body-parser');
 const app = express();
+const userService = require('./services/user_service');
 
-app.listen(PORT, () =>{
-    console.log('Started listening on Port: '+PORT);
+app.listen(PORT, ()=>{
+    console.log(`server listening on port: ${PORT}`);
 });
 
+app.use(bodyParser.json());
 app.get('/', (req, res) =>{
     var date = new Date();
     var timeStamp = date.getDate() + '/'+ date.getMonth() + 1 + '/' + date.getFullYear() + '/';
@@ -14,4 +16,27 @@ app.get('/', (req, res) =>{
     res.json({
         "message" : "Hello "+timeStamp
     });
+});
+
+
+app.post('/user/signup', (req, res) =>{
+   try{
+    userService.insertNewUser(req, res);
+   }catch(e){
+        res.json({
+            "error" : true,
+            "message" : e.message
+        })
+   }
+});
+
+app.post('/user/login', (req, res) =>{
+    try{
+        userService.checkUser(req, res);
+       }catch(e){
+            res.json({
+                "error" : true,
+                "message" : e.message
+            })
+       }
 });
