eff35de447dbcbd92c18374178e7d1ad20b13720
Mihir Sawant <mihirssawant06@gmail.com>
2023-01-04 21:33:32 +0530

    added config user and pass

--- config.js
+++ config.js
@@ -1,1 +1,7 @@
 // all the configs for the app, removed as creating circular dependencies problem
+let emailId = "u2210chauhan@gmail.com";
+let pass = "wjeuddjbgbjswvuq"; // password goes here
+
+module.exports = {
+    emailId, pass
+}
